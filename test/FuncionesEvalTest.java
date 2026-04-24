79301b6160ccd00cd4c1ae72842bb75c403b3eed
GuillermoManuelSalinas25 <salinas.gogui25@cordoba.salesianos.edu>
2026-04-24 17:17:49 +0200

    Se ha añadido la clase principal además de la carpeta de test con su respectiva clase

--- test/FuncionesEvalTest.java
+++ test/FuncionesEvalTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FuncionesEvalTest {
+
+    // Funcion que cuenta cuantas veces aparece un número
     @Test
     void contarApariciones() {
         assertEquals(2, FuncionesEval.contarApariciones(new int[]{1, 2, 3, 1, 4}, 1));
@@ -10,6 +12,7 @@ class FuncionesEvalTest {
         assertEquals(0, FuncionesEval.contarApariciones(new int[]{}, 5));
     }
 
+    // Función que muestra el número mayor en un array
     @Test
     void testEncontrarMayor() {
         int[] array1 = {1, 3, 2, 5, 4};
@@ -26,5 +29,42 @@ class FuncionesEvalTest {
 
         int[] array5 = null;
         assertThrows(IllegalArgumentException.class, () -> FuncionesEval.encontrarMayor(array5));
+    }
+
+    // Función que comprueba si un número es igual a la suma de sus divisores
+    @Test
+    void testEsPerfectoNumeroPerfecto() {
+        assertTrue(FuncionesEval.esPerfecto(6));
+    }
+
+    @Test
+    void testEsPerfectoNumeroNoPerfecto() {
+        assertFalse(FuncionesEval.esPerfecto(8));
+    }
+
+    @Test
+    void testEsPerfectoCero() {
+        assertFalse(FuncionesEval.esPerfecto(0));
+    }
+
+    @Test
+    void testEsPerfectoNegativo() {
+        assertFalse(FuncionesEval.esPerfecto(-6));
+    }
+
+    // Función que devuelve el tipo de clima dependiendo de la temperatura introducida
+    @Test
+    void testTipoClimaFrio() {
+        assertEquals("Frio", FuncionesEval.getTipoClima(5));
+    }
+
+    @Test
+    void testTipoClimaTemplado() {
+        assertEquals("Templado", FuncionesEval.getTipoClima(21));
+    }
+
+    @Test
+    void testTipoClimaCalor() {
+        assertEquals("Calor", FuncionesEval.getTipoClima(36));
     }
 }
